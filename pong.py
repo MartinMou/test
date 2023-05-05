daddd04e0554bfb9f4f9fe98945911fcf6d81798
MartinMou <131851679+MartinMou@users.noreply.github.com>
2023-05-05 13:39:47 +0200

    Nesten ferdig spill
    Små endringer trengs for å gjøre kode mer forståelig, flere komentarer og fikse objekter. Men så langt er spillet funksjonelt

--- pong.py
+++ pong.py
@@ -7,15 +7,17 @@ VINDU_BREDDE = 720
 VINDU_HOYDE  = 480
 vindu = pg.display.set_mode([VINDU_BREDDE, VINDU_HOYDE])
 
+clock = pg.time.Clock()
+
 l1 = []
-for i in range(-600,-400):
+for i in range(-3500,-4000):
     l1.append(i)
 
-for i in range(600,400):
+for i in range(3500,4000):
     l1.append(i)    
 
-tilfeldig_tall = rd.randint(0,200)
-tilfeldig_tall2= rd.randint(0,200)
+tilfeldig_tall = rd.randint(0,500)
+tilfeldig_tall2= rd.randint(0,500)
 
 class Ball:
   """Klasse for å representere en ball"""
@@ -37,34 +39,43 @@ class Hinder(Ball):
         self.xFart = xFart
         self.yFart = yFart
     
+    #resetter ballen tilbake til midten
+    def reset_ball(self):
+        self.x = VINDU_BREDDE // 2
+        self.y = VINDU_HOYDE // 2
+        self.xFart = l1[rd.randint(0,500)]/1000
+        self.yFart = l1[rd.randint(0,500)]/1000
+
     def flytt(self,rectangle1,rectangle2):
         """Metode for å flytte hinderet"""
-        # Sjekker om hinderet er utenfor høyre/venstre kant
-        if ((self.x + self.radius)>=VINDU_BREDDE):
-            self.xFart = -self.xFart
-            rectangle1.increase_score()
-            
-        if ((self.x - self.radius) <= 0):
-            self.xFart = -self.xFart
-            rectangle2.increase_score()
-            
-
-    
+       
         # Sjekker om hinderet er utenfor øvre/nedre kant
         if ((self.y - self.radius) <= 0) or ((self.y + self.radius) >= self.vindusobjekt.get_height()):
             self.yFart = -self.yFart
-
+        #sjekker om hinderet kolliderer med rektanglene
         if ((self.x - self.radius <= 21) and (rectangle1.ypos1 <= self.y <= rectangle1.ypos1 + 120)) or \
             ((self.x + self.radius >= 699) and (rectangle2.ypos2 <= self.y <= rectangle2.ypos2 + 120)):
              self.xFart = -self.xFart
-        
-        
+    
+        #spretter ballen av høyre og venstre kant, oppdaterer score etter hvert sprett
+        #og setter ballen tilbake på midten når spillet er ferdig
+        if ((self.x + self.radius)>=VINDU_BREDDE):
+             self.xFart = -self.xFart
+             rectangle1.increase_score()
+             self.reset_ball()
+        if ((self.x - self.radius) <= 0):
+            self.xFart = -self.xFart
+            rectangle2.increase_score()
+            self.reset_ball()
+
+    
 
         # Flytter hinderet
         self.x += self.xFart
         self.y += self.yFart
         return True
-    
+
+
 class Rectangle:
     def __init__(self,høyde,bredde,farge,vindu,fart):
         self.høyde = høyde
@@ -76,6 +87,7 @@ class Rectangle:
         self.ypos2 = 180
         self.score = 0
     
+    #oppdaterer score til spillerene
     def increase_score(self):
         self.score += 1       
         print(self, self.score)
@@ -85,6 +97,7 @@ class Rectangle:
          pg.draw.rect(vindu,(255,255,255),(1,self.ypos1,20,120))
          pg.draw.rect(vindu,(255,255,255),(699,self.ypos2,20,120))
 
+    #gir metode for å flytte rektanglene
     def flytt(self, taster):
         """Metode for å flytte spilleren"""
         if taster[pg.K_w]:
@@ -95,9 +108,10 @@ class Rectangle:
          self.ypos2 -= self.fart
         if taster[K_DOWN]:
             self.ypos2 += self.fart
-        
-rektangel1 = Rectangle(25,10,(255,255,255),vindu,0.8)
-rektangel2 = Rectangle(25,10,(255,255,255),vindu,0.8)
+
+#gir dimensjoner til objektene
+rektangel1 = Rectangle(25,10,(255,255,255),vindu,5)
+rektangel2 = Rectangle(25,10,(255,255,255),vindu,5)
 hinder = Hinder(360, 240, 10, (255, 255, 255), vindu, l1[tilfeldig_tall]/1000, l1[tilfeldig_tall2]/1000)
 
 fortsett = True
@@ -109,10 +123,10 @@ while fortsett:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             fortsett = False
-    
+    #grenser framerate
+    clock.tick(60)
+    #oppdaterer spillet
     trykkede_taster = pg.key.get_pressed()
-
-    # Farger bakgrunnen lyseblå
     vindu.fill((110, 110, 110))
     rektangel1.tegn()
     rektangel2.tegn()
@@ -126,14 +140,13 @@ while fortsett:
    
     score_display1 = vindu.blit(bilde1,(140,70))
     score_display2 = vindu.blit(bilde2,(520,70))
-    
-
-    
 
     if rektangel2.score == 10:
         print("Spiller 1 vant")
+        pg.quit()
     if rektangel1.score == 10:
         print("spiller 2 vant")
+        pg.quit()
 
     pg.display.flip()
 
